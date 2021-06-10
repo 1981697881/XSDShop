e5ceeada7d2725d5688b516dcdc63ac7d18b45bf
1981697881 <1981697881@qq.com>
2021-06-10 19:55:22 +0800

    Initial commit

---
+++ src/api/system/dept.js
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+
+export function getDepts(params) {
+  return request({
+    url: 'system/dept',
+    method: 'get',
+    params
+  })
+}
+
+export function add(data) {
+  return request({
+    url: 'system/dept',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'system/dept',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function getDeptSuperior(data) {
+  return request({
+    url: 'system/dept',
+    method: 'get',
+    data
+  })
+}
+export default { add, del, getDepts, getDeptSuperior }
